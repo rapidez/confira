e07c6f1d30ac4c5307cd96bab4f08ac8f277cc0a
Roene <roene@justbetter.nl>
2023-12-01 14:15:08 +0100

    Removed h-screen changed it to inset-y-0

--- resources/views/components/slideover.blade.php
+++ resources/views/components/slideover.blade.php
@@ -1,7 +1,7 @@
 @props(['id', 'title' => ''])
 <input type="checkbox" v-bind:id="{{ $id }}" class="peer hidden"/>
 <label v-bind:for="{{ $id }}" class="pointer-events-none fixed inset-0 z-50 cursor-pointer bg-ct-neutral/30 opacity-0 transition peer-checked:pointer-events-auto peer-checked:opacity-100"></label>
-<div class="pointer-events-none fixed top-0 -right-full z-50 flex h-screen w-full max-w-md flex-col bg-white transition-[right] duration-300 peer-checked:pointer-events-auto peer-checked:right-0">
+<div class="pointer-events-none fixed inset-y-0 -right-full z-50 flex w-full max-w-md flex-col bg-white transition-[right] duration-300 peer-checked:pointer-events-auto peer-checked:right-0">
     <div class="flex w-full items-center justify-center bg-ct-primary py-4 px-5 text-lg font-bold text-white">
         <div>{{ $title }}</div>
         <label v-bind:for="{{ $id }}" class="absolute right-5 cursor-pointer">
