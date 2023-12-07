db576e19dabbea14df3a4d26a907f8eb1c9f16f8
Roene <roene@justbetter.nl>
2023-12-07 17:10:42 +0100

    Changed items to ul > li | Added alt text for button

--- resources/views/components/summary.blade.php
+++ resources/views/components/summary.blade.php
@@ -1,38 +1,40 @@
-<x-rapidez-ct::separated-listing tag="dl">
-    <toggler open>
-        <div class="flex flex-col" slot-scope="{ isOpen, toggle }" >
-            <div @click="toggle" class="flex w-full justify-between">
-                <dt class="flex gap-x-1">
-                    <span>@lang('Total products') (@{{ Math.round(cart.items_qty) }})</span>
-                    <x-heroicon-o-chevron-down class="text-inactive w-3 stroke-2 transition-all" ::class="{ 'rotate-180': isOpen }" />
-                </dt>
-                <dd class="font-medium text-ct-neutral">@{{ cart.subtotal | price }}</dd>
+<x-rapidez-ct::separated-listing>
+    <li>
+        <toggler open>
+            <div class="flex flex-col w-full" slot-scope="{ isOpen, toggle }" >
+                <div @click="toggle" class="flex w-full justify-between">
+                    <div class="flex gap-x-1">
+                        <span>@lang('Total products') (@{{ Math.round(cart.items_qty) }})</span>
+                        <x-heroicon-o-chevron-down class="text-inactive w-3 stroke-2 transition-all" ::class="{ 'rotate-180': isOpen }" />
+                    </div>
+                    <div class="font-medium text-ct-neutral">@{{ cart.subtotal | price }}</div>
+                </div>
+                <div v-show="isOpen" v-for="item in cart.items" class="text-ct-inactive">
+                    <span class="text-ct-neutral">@{{ item.qty }}x</span>
+                    @{{ item.name }}
+                </div>
             </div>
-            <div v-show="isOpen" v-for="item in cart.items" class="text-ct-inactive">
-                <span class="text-ct-neutral">@{{ item.qty }}x</span>
-                @{{ item.name }}
-            </div>
-        </div>
-    </toggler>
-    <div v-if="cart.tax > 0">
-        <dt>@lang('Tax')</dt>
-        <dd class="font-medium text-ct-neutral">@{{ cart.tax | price }}</dd>
-    </div>
-    <div v-if="cart.discount_name">
-        <dt>@lang('Discount') (@{{ cart.discount_name }})</dt>
-        <dd class="text-ct-primary font-medium">@{{ cart.discount_amount | price }}</dd>
-    </div>
-    <div v-if="cart.shipping_method">
-        <dt>@lang('Shipping')</dt>
-        <dd v-if="cart.shipping_amount_excl_tax > 0 " class="font-medium text-ct-neutral">
+        </toggler>
+    </li>
+    <li v-if="cart.tax > 0">
+        <div>@lang('Tax')</div>
+        <div class="font-medium text-ct-neutral">@{{ cart.tax | price }}</div>
+    </li>
+    <li v-if="cart.discount_name">
+        <div>@lang('Discount') (@{{ cart.discount_name }})</div>
+        <div class="text-ct-primary font-medium">@{{ cart.discount_amount | price }}</div>
+    </li>
+    <li v-if="cart.shipping_method">
+        <div>@lang('Shipping')</div>
+        <div v-if="cart.shipping_amount_excl_tax > 0 " class="font-medium text-ct-neutral">
             @{{ cart.shipping_amount_excl_tax | price }}
-        </dd>
-        <dd v-else class="font-medium text-ct-neutral">
+        </div>
+        <div v-else class="font-medium text-ct-neutral">
             @lang('Free')
-        </dd>
-    </div>
-    <div class="border-t border-dashed mt-2">
-        <dt class="text-base text-ct-neutral font-medium">@lang('Total price')</dt>
-        <dd class="font-bold text-xl text-ct-neutral">@{{ cart.total | price }}</dd>
-    </div>
+        </div>
+    </li>
+    <li class="border-t border-dashed mt-2">
+        <div class="text-base text-ct-neutral font-medium">@lang('Total price')</div>
+        <div class="font-bold text-xl text-ct-neutral">@{{ cart.total | price }}</div>
+    </li>
 </x-rapidez-ct::separated-listing>
