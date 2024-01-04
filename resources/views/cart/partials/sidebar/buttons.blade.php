65e67f9986a0cc2d91d1e752e7564aff738f1dc8
Roene <roene@justbetter.nl>
2024-01-04 11:13:25 +0100

    Moved buttons to own file to make override easier

---
+++ resources/views/cart/partials/sidebar/buttons.blade.php
@@ -0,0 +1,4 @@
+<x-rapidez-ct::button.enhanced :href="route('checkout')" class="flex w-full items-center justify-center gap-1 mt-6" dusk="checkout">
+    @lang('To checkout')
+</x-rapidez-ct::button.enhanced>
+@include('rapidez-ct::cart.coupon')
--- resources/views/cart/partials/sidebar/summary.blade.php
+++ resources/views/cart/partials/sidebar/summary.blade.php
@@ -4,9 +4,6 @@
 
 <x-rapidez-ct::summary />
 
-<x-rapidez-ct::button.enhanced :href="route('checkout')" class="flex w-full items-center justify-center gap-1 mt-6" dusk="checkout">
-    @lang('To checkout')
-</x-rapidez-ct::button.enhanced>
-@include('rapidez-ct::cart.coupon')
+@include('rapidez-ct::cart.partials.sidebar.buttons')
 @include('rapidez-ct::cart.partials.sidebar.payment')
 @include('rapidez-ct::cart.partials.sidebar.usps')
