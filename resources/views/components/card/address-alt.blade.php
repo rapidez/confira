f47f0c890afe9b656ae5de706de28b2effe74a35
indy koning <indy@justbetter.nl>
2024-01-08 13:44:56 +0100

    Show a button to select address + frontend validation if default address is missing

--- resources/views/components/card/address-alt.blade.php
+++ resources/views/components/card/address-alt.blade.php
@@ -3,5 +3,8 @@
 <x-rapidez-ct::card.white class="!border-0 !shadow-none !p-0" {{ $attributes->only('v-if') }}>
     <x-rapidez-ct::address {{ $attributes }} :$customTitle>
         {{ $slot }}
+        <x-slot:empty>
+            {{ $empty ?? '' }}
+        </x-slot>
     </x-rapidez-ct::address>
 </x-rapidez-ct::card.white>
