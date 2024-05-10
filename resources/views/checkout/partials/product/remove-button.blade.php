1b04cb41a6547bc17cf66b5c83b933048a26b619
Jade Geels <jade@justbetter.nl>
2024-05-10 11:39:07 +0200

    Fix remove-button

---
+++ resources/views/checkout/partials/product/remove-button.blade.php
@@ -0,0 +1,12 @@
+<graphql-mutation
+    :query="'mutation ($cart_id: String!, $cart_item_id: Int) { removeItemFromCart(input: { cart_id: $cart_id, cart_item_id: $cart_item_id }) { cart { ' + config.queries.cart + ' } } }'"
+    :variables="{ cart_id: mask, cart_item_id: item.id }"
+    :notify="{ message: item.product.name+' '+config.translations.cart.remove }"
+    :callback="updateCart"
+    :error-callback="checkResponseForExpiredCart"
+    v-slot="{ mutate }"
+>
+    <button v-on:click="mutate" class="text-ct-inactive mt-1 text-xs hover:underline" :dusk="'item-delete-' + index" title="{{ __('Remove') }}">
+        <x-heroicon-o-trash class="text-inactive w-5"/>
+    </button>
+</graphql-mutation>
