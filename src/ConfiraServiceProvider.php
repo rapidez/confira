b925c0d8c72d642fa992b1399a0d24e882443739
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-10-21 10:34:01 +0200

    Add dutch translations

--- src/ConfiraServiceProvider.php
+++ src/ConfiraServiceProvider.php
@@ -17,7 +17,9 @@ class ConfiraServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->bootPublishables();
+        $this
+            ->bootPublishables()
+            ->bootTranslations();
     }
 
     public function bootViews() : self
@@ -36,6 +38,13 @@ class ConfiraServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/rapidez/confira.php' => config_path('rapidez/confira.php'),
         ], 'rapidez-confira-config');
+
+        return $this;
+    }
+
+    protected function bootTranslations(): self
+    {
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../lang');
 
         return $this;
     }
