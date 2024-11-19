1f6fcd68ce915e60d7c67cf5c7a13519b6af5be2
Roy Duineveld <royduineveld@gmail.com>
2024-11-19 11:48:31 +0100

    Use the resizedPath helper

--- resources/views/components/product/partials/image.blade.php
+++ resources/views/components/product/partials/image.blade.php
@@ -1,4 +1,9 @@
 <div class="flex shrink-0 h-28 w-28 !p-0">
-    <img v-if="item.product.image" class="object-contain" :alt="item.product.name" :src="'/storage/{{ config('rapidez.store') }}/resizes/200/magento' + item.product.image.url.replace(config.media_url, '') + '.webp'">
+    <img
+        v-if="item.product.image"
+        class="object-contain"
+        :alt="item.product.name"
+        :src="resizedPath(item.product.image.url + '.webp', '200')"
+    />
     <x-rapidez::no-image v-else />
 </div>
