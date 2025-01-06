29ae511bba9654660f5d5b87c8d665cf3971f492
Jade Geels <jade@justbetter.nl>
2025-01-06 10:01:30 +0100

    Carry attributes over to select input

--- resources/views/components/input/select/country.blade.php
+++ resources/views/components/input/select/country.blade.php
@@ -1,6 +1,6 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <div v-if="data" slot-scope="{ data }">
-        <x-rapidez-ct::input.select :label="__('Country')">
+        <x-rapidez-ct::input.select :$attributes :label="__('Country')">
             <option v-for="country in data.countries" :value="country.two_letter_abbreviation.toUpperCase()">
                 @{{ country.full_name_locale }}
             </option>
