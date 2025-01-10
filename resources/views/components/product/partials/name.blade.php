8f33ad854a9f2e1083107ff60d904f75d2227d69
Roene <roene@justbetter.nl>
2025-01-10 11:00:51 +0100

    2.x - Remove border right on last item | Remove _

--- resources/views/components/product/partials/name.blade.php
+++ resources/views/components/product/partials/name.blade.php
@@ -2,7 +2,7 @@
     <a class="hover:underline" :href="item.product.url_key + item.product.url_suffix | url">
         <div class="font-semibold text-sm" dusk="cart-item-name">@{{ item.product.name }}</div>
     </a>
-    <div class="flex flex-wrap *:border-r -mx-2 *:px-2 text-inactive text-xs subpixel-antialiased mt-2 *:mb-2 *:leading-3">
+    <div class="flex flex-wrap *:border-r last:*:border-r-0 -mx-2 *:px-2 text-inactive text-xs subpixel-antialiased mt-2 *:mb-2 *:leading-3">
         <div v-for="option in item.configurable_options">
                 @{{ option.value_label }}
             </div>
@@ -10,7 +10,7 @@
             @{{ option.label }}: @{{ option.values[0].label || option.values[0].value }}
         </div>
         <div v-for="option in config.cart_attributes" v-if="item.product.attribute_values?.[option] && typeof item.product.attribute_values[option] === 'object'">
-            @{{ option }}: <span v-html="item.product.attribute_values[option]?.join(', ')"></span>
+            <p class="normal-case first-letter:uppercase">@{{ option.replace(/_/g, ' ') }}: <span v-html="item.product.attribute_values[option]?.join(', ')"></span></p>
         </div>
     </div>
 </div>
