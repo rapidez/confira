3eb19d75f17f46c00f850a29f08549b9a847f800
Jade Geels <jade@justbetter.nl>
2025-06-13 14:56:59 +0200

    Fix small issues with shipping methods

--- resources/views/components/input/select/country.blade.php
+++ resources/views/components/input/select/country.blade.php
@@ -1,8 +1,8 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <div v-if="data" slot-scope="{ data }">
         <x-rapidez-ct::input.select :$attributes :label="__('Country')">
-            <option v-for="country in data.countries.sort((a, b) => a.full_name_locale.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
-                @{{ country.full_name_locale }}
+            <option v-for="country in data.countries.toSorted((a, b) => a.full_name_locale?.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
+                @{{ country.full_name_locale || country.two_letter_abbreviation }}
             </option>
         </x-rapidez-ct::input.select>
     </div>
