d1ccf477ce7654ce24dd732aeb380832f81d2e24
Jade Geels <jade@justbetter.nl>
2025-06-19 15:20:41 +0200

    Fix small issues with shipping methods

--- resources/views/components/summary.blade.php
+++ resources/views/components/summary.blade.php
@@ -17,7 +17,7 @@
         <div>@{{ discount.label }}</div>
         <div class="text-green-700 font-semibold">@{{ discount.amount.value | price }}</div>
     </li>
-    <li v-if="{{ $type }}?.shipping_method">
+    <li v-if="{{ $type }}?.shipping_method || {{ $type }}?.shipping_addresses[0]?.selected_shipping_method?.amount">
         <div>@lang('Shipping')</div>
         <div v-if="{{ $type }}?.total?.total_shipping.value > 0 || {{ $type }}?.shipping_addresses[0]?.selected_shipping_method?.amount?.value > 0" class="font-medium">
             <span v-text="$options.filters.price({{ $type }}?.total?.total_shipping?.value ?? {{ $type }}?.shipping_addresses[0]?.selected_shipping_method?.amount?.value)"></span>
