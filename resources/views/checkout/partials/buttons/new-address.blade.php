6d9d9619d9abbd0b9bb61d89f797b631d036c6c3
Jade Geels <a@jgw.tf>
2025-09-09 13:38:32 +0200

    Simplify address cards (#49)

--- resources/views/checkout/partials/buttons/new-address.blade.php
+++
@@ -1,4 +0,0 @@
-<button v-on:click.prevent="toggleEdit" class="flex flex-col items-center justify-center gap-y-2 text-muted font-medium bg rounded-md max-md:hidden">
-    <span>+</span>
-    <span>@lang('Add new address')</span>
-</button>
