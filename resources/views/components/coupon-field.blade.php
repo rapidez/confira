5e40b72115f0727762ea8c18e85042fd530387d0
Jade <jade@justbetter.nl>
2025-09-12 14:46:46 +0200

    Fix coupons

--- resources/views/components/coupon-field.blade.php
+++ resources/views/components/coupon-field.blade.php
@@ -1,11 +1,10 @@
 <graphql-mutation
-    :query="'mutation ($cart_id: String!, $coupon_code: String!) { applyCouponToCart(input: { cart_id: $cart_id, coupon_code: $coupon_code })  { ...cart } } ' + config.fragments.cart"
+    :query="'mutation ($cart_id: String!, $coupon_code: String!) { applyCouponToCart(input: { cart_id: $cart_id, coupon_code: $coupon_code })  { cart { ...cart } } } ' + config.fragments.cart"
     :variables="{ cart_id: mask, coupon_code: '' }"
     :notify="{ message: config.translations.cart.coupon.applied }"
     :clear="true"
     :watch="false"
     :callback="updateCart"
-    :error-callback="checkResponseForExpiredCart"
     v-slot="{ mutate, variables }"
 >
     <form v-on:submit.prevent="mutate" class="flex w-full gap-y-3 flex-col">
