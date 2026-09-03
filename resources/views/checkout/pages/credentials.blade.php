c198de93affeab51561ca21768f184a84bc054a4
indy koning <indy@justbetter.nl>
2026-09-03 09:40:45 +0200

    Added PR number

--- resources/views/checkout/pages/credentials.blade.php
+++ resources/views/checkout/pages/credentials.blade.php
@@ -31,7 +31,7 @@
                             @lang('Back to cart')
                         </x-rapidez::button.outline>
 
-                        <x-rapidez::button.conversion data-testid="continue" loader v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action --}}">
+                        <x-rapidez::button.conversion data-testid="continue" loader v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action (#85) --}}">
                             @lang('Next')
                         </x-rapidez::button.conversion>
                     </x-rapidez-ct::toolbar>
--- resources/views/checkout/pages/login.blade.php
+++ resources/views/checkout/pages/login.blade.php
@@ -33,7 +33,7 @@
                             @lang('Back to cart')
                         </x-rapidez::button.outline>
 
-                        <x-rapidez::button.conversion loader type="submit" data-testid="continue" v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action (#250) --}}">
+                        <x-rapidez::button.conversion loader type="submit" data-testid="continue" v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action (#85) --}}">
                             @lang('Next')
                         </x-rapidez::button.conversion>
                     </x-rapidez-ct::toolbar>
